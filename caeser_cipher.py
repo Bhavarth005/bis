5d8632275e643af038d40cd5267d2c5b4dba7a77
Bhavarth <abcbhavarth@gmail.com>
2024-02-16 13:05:53 +0530

    added decipher

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -10,3 +10,11 @@ pt = input("Enter plain text: ")
 key = int(input("Enter key: "))
 ct = cipher(pt, key)
 print(ct)
+
+def decipher(ct, key):
+    pt=[]
+
+    for char in ct:
+        ct.append(chr(ord(char) - key))
+
+    return "".join(pt)
